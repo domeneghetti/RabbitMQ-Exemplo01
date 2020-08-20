12e950eeec2111105c048e7ee08be36dcbdf76a7
Fernando Miranda Domeneghetti <fernandomirandadomeneghetti@hotmail.com>
2020-08-19 21:18:37 -0300

    Envio do fonte ao Git

---
+++ RabbitMqPublish/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace RabbitMq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(@"amqp://guest:guest@127.0.0.1:5672/teste"),
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+                AutomaticRecoveryEnabled = true
+            };
+
+            using(var connection = connectionFactory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "testeQueueEx", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                    channel.ExchangeDeclare(exchange: "amq.fanout", type: "fanout", durable: true, autoDelete: false);
+
+                    byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes("{{ nome:\"Fernando\", dataNascimento:\"29-07\" }}");
+                    
+                    channel.BasicPublish(exchange: "amq.fanout",
+                                         routingKey: "",
+                                         basicProperties: null,
+                                         body: messageBodyBytes);
+
+                    Console.WriteLine("Publish!");
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMqRead/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+namespace RabbitMqRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(@"amqp://guest:guest@127.0.0.1:5672/teste"),
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+                AutomaticRecoveryEnabled = true
+            };
+
+            using(var connection = connectionFactory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "testeQueueEx", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine("[x] Message: {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "testeQueueEx", autoAck: true, consumer: consumer);
+                };
+            };
+
+            Console.WriteLine("Read!");
+        }
+    }
+}
