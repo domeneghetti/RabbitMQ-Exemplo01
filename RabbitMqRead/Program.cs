f806de60cf081e6b475f56de0dfd9bd31ce0fd37
Fernando Miranda Domeneghetti <fernandomirandadomeneghetti@hotmail.com>
2020-09-01 21:34:01 -0300

    ajuste do RabbitMQRead

--- RabbitMqRead/Program.cs
+++ RabbitMqRead/Program.cs
@@ -2,8 +2,6 @@
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-using System.Threading;
-using System.Threading.Tasks;
 
 
 namespace RabbitMqRead
@@ -12,42 +10,36 @@ namespace RabbitMqRead
     {
         static void Main(string[] args)
         {
-             var t = Task.Factory.StartNew(() =>
-             {
-                ConnectionFactory connectionFactory = new ConnectionFactory()
-                {
-                    Uri = new Uri(@"amqp://guest:guest@127.0.0.1:5672/"),
-                    NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
-                    AutomaticRecoveryEnabled = true
-                };
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(@"amqp://guest:guest@127.0.0.1:5672/"),
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+                AutomaticRecoveryEnabled = true
+            };
 
-                using(var connection = connectionFactory.CreateConnection())
+            using(var connection = connectionFactory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
                 {
-                    using(var channel = connection.CreateModel())
+                    channel.QueueDeclare(queue: "testeQueueEx", 
+                                        durable: true, 
+                                        exclusive: false, 
+                                        autoDelete: false, 
+                                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
                     {
-                        channel.QueueDeclare(queue: "testeQueueEx", 
-                                            durable: true, 
-                                            exclusive: false, 
-                                            autoDelete: false, 
-                                            arguments: null);
-
-                        var consumer = new EventingBasicConsumer(channel);
-                        consumer.Received += (model, ea) =>
-                        {
-                            var body = ea.Body;
-                            var message = Encoding.UTF8.GetString(body.ToArray());
-                            Console.WriteLine("[x] Message: {0}", message);
-                        };
-
-                        channel.BasicConsume(queue: "testeQueueEx", autoAck: true, consumer: consumer);
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine("[x] Message: {0}", message);
                     };
+
+                    channel.BasicConsume(queue: "testeQueueEx", autoAck: true, consumer: consumer);
                 };
+            };
 
-                Console.WriteLine("Read!");
-                Console.ReadKey();
-             });
-
-            
+            Console.WriteLine("Read!");
         }
     }
 }
